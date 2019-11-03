a0c09184c388f12b482c89e15d2cc977f7f004fd
安東 <takuya0411@gmail.com>
2019-11-04 08:24:17 +0900

    ラベルを環境変数から持ってくることで汎用的に使えるようにした

--- src/run_classifier.py
+++ src/run_classifier.py
@@ -18,6 +18,8 @@ import tokenization_sentencepiece as tokenization
 import tensorflow as tf
 import utils
 
+import pandas as pd
+
 CURDIR = os.path.dirname(os.path.abspath(__file__))
 CONFIGPATH = os.path.join(CURDIR, os.pardir, 'config.ini')
 config = configparser.ConfigParser()
@@ -211,42 +213,43 @@ class DataProcessor(object):
       return lines
 
 
-class LivedoorProcessor(DataProcessor):
-  """Processor for the livedoor data set (see https://www.rondhuit.com/download.html)."""
+class GeneralProcessor(DataProcessor):
+
+  def read_tsv(self, path):
+    df = pd.read_csv(path, sep="\t")
+    return [(str(text), str(label)) for text,label in zip(df['text'], df['label'])]
+
 
   def get_train_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+        self.read_tsv(os.path.join(data_dir, "train.tsv")), "train")
 
   def get_dev_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+        self.read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
 
   def get_test_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+      self.read_tsv(os.path.join(data_dir, "test.tsv")), "test")
 
   def get_labels(self):
     """See base class."""
-    return ['dokujo-tsushin', 'it-life-hack', 'kaden-channel', 'livedoor-homme', 'movie-enter', 'peachy', 'smax', 'sports-watch', 'topic-news']
+    return os.environ["LABEL"].split(',') 
 
   def _create_examples(self, lines, set_type):
     """Creates examples for the training and dev sets."""
     examples = []
     for (i, line) in enumerate(lines):
-      if i == 0:
-        idx_text = line.index('text')
-        idx_label = line.index('label')
-      else:
-        guid = "%s-%s" % (set_type, i)
-        text_a = tokenization.convert_to_unicode(line[idx_text])
-        label = tokenization.convert_to_unicode(line[idx_label])
-        examples.append(
-            InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+      guid = "%s-%s" % (set_type, i)
+      text_a = tokenization.convert_to_unicode(line[0])
+      label = tokenization.convert_to_unicode(line[1])
+      examples.append(
+          InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
     return examples
+
 
 
 def convert_single_example(ex_index, example, label_list, max_seq_length,
@@ -659,7 +662,7 @@ def main(_):
   tf.logging.set_verbosity(tf.logging.INFO)
 
   processors = {
-      "livedoor": LivedoorProcessor,
+      "general": GeneralProcessor
   }
 
   tokenization.validate_case_matches_checkpoint(FLAGS.do_lower_case,
